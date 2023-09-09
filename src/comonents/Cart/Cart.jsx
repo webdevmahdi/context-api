94f8cb47c8e5d3fb7e340cabbee3377c22c42d3c
webdevmahdi <mahdihasan01705259@gmail.com>
2023-09-09 22:12:06 +0600

    cart added at home

--- src/comonents/Cart/Cart.jsx
+++ src/comonents/Cart/Cart.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 const Cart = () => {
   return (
     <div>
+      <h1>Cart</h1>
     </div>
   )
 }
--- src/comonents/Home/Home.jsx
+++ src/comonents/Home/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Home.css'
+import Cart from '../Cart/Cart';
 
 const Home = () => {
   return (
-    <div className='card'>
+    <div className="products">
+      <div className='card'>
         <h1>Mahdi hasan</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa similique obcaecati incidunt ad, quam magnam in. Quam deserunt consequuntur facere possimus? Iure eum reprehenderit mollitia labore. Voluptatum voluptatem incidunt ex.</p>
-        <button>Add to cart</button>
+        <div className='button'>
+          <button>Add to cart</button>
+          <button>Details</button>
+        </div>
+      </div>
+      <Cart></Cart>
     </div>
   )
 }
